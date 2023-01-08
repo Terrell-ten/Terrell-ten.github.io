199a6f482b60f77ea0c80cbf5671573bf170212d
Terrell-ten <745366596@qq.com>
2023-01-08 22:46:27 +0800

    启动失效功能

--- api.js
+++ api.js
@@ -1,2 +1,6 @@
  // val bgg = 'app';
 console.log('bgg');
+// document.getElementsByTagName('body')[0].innerHTML='' // 清楚内容
+if( document.domain=='jxcsys.520shq.com'||document.domain=='pf.520shq.com'||document.domain=='pfs.520shq.com'){
+  document.getElementsByTagName('body')[0].innerHTML='';
+}
