68855fbcbc3520cfaa37c38f1df5b622e2d40e85
Terrell-ten <745366596@qq.com>
2023-01-08 22:47:30 +0800

    启动到期功能

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
  // val bgg = 'app';
-console.log('bgg');
+// console.log('bgg');
 // document.getElementsByTagName('body')[0].innerHTML='' // 清楚内容
 if( document.domain=='jxcsys.520shq.com'||document.domain=='pf.520shq.com'||document.domain=='pfs.520shq.com'){
   document.getElementsByTagName('body')[0].innerHTML='';
