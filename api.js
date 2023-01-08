6638ec5ba9647e629c4fa470294f41a874d64abf
Terrell-ten <745366596@qq.com>
2023-01-08 22:57:54 +0800

    Update api.js

--- api.js
+++ api.js
@@ -2,5 +2,7 @@
 // console.log('bgg');
 // document.getElementsByTagName('body')[0].innerHTML='' // 清楚内容
 if( document.domain=='jxcsys.520shq.com'||document.domain=='pf.520shq.com'||document.domain=='pfs.520shq.com'){
-  document.getElementsByTagName('body')[0].innerHTML='';
+  
+ 
+  //document.getElementsByTagName('body')[0].innerHTML='';
 }
