b8af2cd6fa309bd01aa7102a69033ab02293c922
Terrell-ten <745366596@qq.com>
2023-03-20 21:26:41 +0800

    bufenjinyong

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
  // val bgg = 'app';
 // console.log('bgg');
-// document.getElementsByTagName('body')[0].innerHTML='' // 清楚内容
-if( document.domain=='jxcsys.520shq.com'||document.domain=='pf.520shq.com'||document.domain=='pfs.520shq.com'){
+// document.getElementsByTagName('body')[0].innerHTML='' // 清楚内容||document.domain=='pfs.520shq.com'||document.domain=='jxcsys.520shq.com'
+if( document.domain=='pf.520shq.com'){
   
  
   document.getElementsByTagName('body')[0].innerHTML='';
